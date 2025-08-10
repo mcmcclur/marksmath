7601a791605bbfb6b7586f9ebbebea0d420322a7
Mark McClure <mcmcclur@unca.edu>
2025-08-09 21:45:43 -0400

    A little more structure to the embed

--- EmbedDiscourseMathTopics/embedDiscourseMathTopic.js
+++ EmbedDiscourseMathTopics/embedDiscourseMathTopic.js
@@ -3,8 +3,19 @@ const MathJax = importMathJax();
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
 
 export async function embedDiscourseMathTopic(topicId) {
-  const replies_container = d3.create('div');
-  replies_container.attr('class', 'replies-container');
+  const replies_container = d3.create('div')
+    .attr('class', 'replies-container');
+  const reply_button_container = replies_container
+    .append('div')
+    .attr('class', 'button-container')
+    .style('margin', '5px');
+  const reply_link = reply_button_container
+    .append('a')
+    .attr('href', `https://discourse.marksmath.org/t/${topicId}`);
+  reply_link
+    .append('button')
+    .text('Reply on Discourse');
+
   let page = 1;
   try {
     while(page < 20) {
@@ -16,7 +27,12 @@ export async function embedDiscourseMathTopic(topicId) {
       }
       
       const json = await response.json();
-      const posts = json.post_stream.posts;
+      console.log(json);
+
+      let posts = json.post_stream.posts;
+      if(page == 1) {
+        posts = posts.slice(1);
+      }
       
       if (posts.length <= 1) {
         throw new Error('No posts found in the topic.');
@@ -26,12 +42,20 @@ export async function embedDiscourseMathTopic(topicId) {
         if (!post.cooked) {
           throw new Error('Post content is missing.');
         }
-      const div = replies_container
+        const reply_container = replies_container
           .append('div')
+          .attr('class', 'reply-container')
           .style('border-top', 'solid 0.5px black')
-          .style('border-bottom', 'solid 0.5px black');
-        div.html(post.cooked);
-        div.selectAll('span.math')
+          // .style('border-bottom', 'solid 0.5px black');
+        
+        // Next step is to an info div with the author, date, 
+        // and potentially reply to whom info.  
+
+        const post_container = reply_container
+          .append('div')
+          .attr('class', 'post-container');
+        post_container.html(post.cooked);
+        post_container.selectAll('span.math')
           .nodes()
           .forEach(function(span) {
             const d3Span = d3.select(span);
@@ -44,7 +68,7 @@ export async function embedDiscourseMathTopic(topicId) {
             d3Span.append(() => new_content);
             d3Span.attr('class', 'math');
           });
-        div.selectAll('div.math')
+        post_container.selectAll('div.math')
           .nodes()
           .forEach(function(Div) {
             const d3Div = d3.select(Div);
