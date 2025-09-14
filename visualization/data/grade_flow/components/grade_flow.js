48dda8e9b45da3841b38d21246d7f0b361832d62
Mark McClure <mcmcclur@unca.edu>
2025-09-14 16:55:51 -0400

    Fixed Spring 2024 data for the grade_flow vis

--- visualization/data/grade_flow/components/grade_flow.js
+++ visualization/data/grade_flow/components/grade_flow.js
@@ -1,10 +1,10 @@
 // Grade flow
+// A visualization illustrating how grades have flowed
+// through my classes.
 // Mark McClure
 // Original version July, 2019
 // Updated September, 2025
-
-// A visualization illustrating how grades have flowed
-// through my classes.
+// Which is why it generally looks so crappy
 
 
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
@@ -59,84 +59,84 @@ function setup() {
         d3.select('#student_count').text(student_count)
         //d3.select('#course_count').text(course_count)
 
-    // get_count computes the numbers displayed over the buttons
-    // on mouseenter. It accepts a list of filters that look like:
-    // [
-    //   {
-    //     'grade': grade,
-    //     'column_name': column_name
-    //   }, ...
-    // ]
-    get_count = function(filters) {
-        var cases = data;
-        var local_filters = filters;
-        if(global_filter) {
-            local_filters.push(global_filter);
-        }
-        local_filters.forEach(function(d) {
-            var column_to_check = d.column_name;
-            var grade_to_check_for = d.grade;
-            cases = cases.filter(function(dd) {
-                return (dd[column_to_check].slice(0,1) == grade_to_check_for)
+        // get_count computes the numbers displayed over the buttons
+        // on mouseenter. It accepts a list of filters that look like:
+        // [
+        //   {
+        //     'grade': grade,
+        //     'column_name': column_name
+        //   }, ...
+        // ]
+        get_count = function(filters) {
+            var cases = data;
+            var local_filters = filters;
+            if(global_filter) {
+                local_filters.push(global_filter);
+            }
+            local_filters.forEach(function(d) {
+                var column_to_check = d.column_name;
+                var grade_to_check_for = d.grade;
+                cases = cases.filter(function(dd) {
+                    return (dd[column_to_check].slice(0,1) == grade_to_check_for)
+                })
             })
-        })
-        return cases.length;
-    }
+            return cases.length;
+        }
 
-  // Compute some counts to display in the page.
-  var b_count = get_count([
-    {
-      'grade': 'B',
-      'column_name': 'Grade 1'
-    }
-  ]);
-  var b_then_a_count = get_count([
-    {
-      'grade': 'B',
-      'column_name': 'Grade 1'
-    },
-    {
-      'grade': 'A',
-      'column_name': 'Final Grade'
-    }
-  ]);
-  d3.select('#b_count').text(b_count)
-  d3.select('#b_then_a_count').text(b_then_a_count)
+        // Compute some counts to display in the page.
+        var b_count = get_count([
+            {
+            'grade': 'B',
+            'column_name': 'Grade 1'
+            }
+        ]);
+        var b_then_a_count = get_count([
+            {
+            'grade': 'B',
+            'column_name': 'Grade 1'
+            },
+            {
+            'grade': 'A',
+            'column_name': 'Final Grade'
+            }
+        ]);
+        d3.select('#b_count').text(b_count)
+        d3.select('#b_then_a_count').text(b_then_a_count)
 
-  // Function to add a link connecting button 1 and button 2,
-  // accounting for the selected button via the global_filter.
-  add_link = function(grade1, grade2, column1, column2) {
-    var button_info1 = button_info.filter(function(b) {
-      return (b.grade == grade1) && (b.column_name == column1)
-    })[0];
-    var button_info2 = button_info.filter(function(b) {
-      return (b.grade == grade2) && (b.column_name == column2)
-    })[0];
-    var count = get_count([
-      {
-        'grade': grade1,
-        'column_name': column1
-      },
-      {
-        'grade': grade2,
-        'column_name': column2
-      }
-    ]);
-    if(count > 0) {
-      link_data.push({
-        'source': {
-          'x': button_info1.x, // + button_width/2,
-          'y': button_info1.y //  - button_height/2
-        },
-        'target': {
-          'x': button_info2.x, // - button_width/2,
-          'y': button_info2.y // + button_height/2
-        },
-        'width': 10*Math.sqrt(count/total)
-      })
-    }
-  }
-});
+        // Function to add a link connecting button 1 and button 2,
+        // accounting for the selected button via the global_filter.
+        add_link = function(grade1, grade2, column1, column2) {
+            var button_info1 = button_info.filter(function(b) {
+            return (b.grade == grade1) && (b.column_name == column1)
+            })[0];
+            var button_info2 = button_info.filter(function(b) {
+            return (b.grade == grade2) && (b.column_name == column2)
+            })[0];
+            var count = get_count([
+            {
+                'grade': grade1,
+                'column_name': column1
+            },
+            {
+                'grade': grade2,
+                'column_name': column2
+            }
+            ]);
+            if(count > 0) {
+            link_data.push({
+                'source': {
+                'x': button_info1.x, // + button_width/2,
+                'y': button_info1.y //  - button_height/2
+                },
+                'target': {
+                'x': button_info2.x, // - button_width/2,
+                'y': button_info2.y // + button_height/2
+                },
+                'width': 10*Math.sqrt(count/total)
+            })
+            }
+        }
+    });
 
 // Set up the container and SVG
 // The container will hold some buttons that are laid on
@@ -188,133 +188,138 @@ var button_info = grade_matrix.map(function(g,i) {
 });
 
 
-// Set up the descriptive text with location based on just
-// the first four terms in button_info.
-svg
-  .style('overflow', 'visible')
-  .append('g')
-  .selectAll('text')
-  .data(button_info.slice(0,4))
-  .enter().append('text')
-  .attr('fill', 'currentColor')
-  .attr('text-anchor', 'middle')
-  .attr('x', d => d.x-10)
-  .attr('y', text_pad)
-  .attr("text-decoration","underline")
-  .text(function(o,i) {
-    if(i < 3){
-      return 'Grade ' + (i+1).toString()
-    }
-    else {
-      return 'Final Grade'
-    }
-  });
-
-// Lay the buttons down within the container on
-// top of the SVG.
-container
-  .selectAll('button') 
-  .data(button_info)
-  .enter().append('button')
-  .attr('class', 'btn btn-secondary')
-  .style('position', 'absolute')
-  .style('top', d => `${(d.y - button_height/2)}px`)
-  .style('left', d => `${(d.x - button_width/2)}px`)
-//   .style('padding-left', '15px')
-//   .style('padding-right', '15px')
-  .style('height', `${button_height}px`)
-   .style('width', '44px')
-  .text(d => d.grade)
-  .attr('id', d => d.id)
-
-  // Respone to mouseenter.
-  .on('mouseenter', function(e, d) {
-    // Figure out which button we're on, use it to define the
-    // global_filter, the total number of selected students, and
-    // the button_info itself (for placing the tooltip).
-    var grade = d.grade;
-    var selected_column = d.column_name;
-    global_filter = {'grade': grade, 'column_name': selected_column};
-    total = get_count([global_filter]);
-    var selected_button = button_info.filter(function(bi) {
-      return ((bi.grade == grade) &&
-        (bi.column_name == selected_column))
-    })[0];
-
-    // Add the tooltip for the selected button.
-    container.append("div")
-        .attr("class", "tooltip")
-        .html(total)
-        .style('left', `${selected_button.x + 0.35*button_width}px`)
-        .style('top', `${selected_button.y - 0.5*button_height}px`)
-
-    // Compute and add the links. It's small enough that we'll
-    // just iterate over all 3x6x6=108 possible links and add
-    // one only when the count is positive.
-    var column_pairs = [
-      ['Grade 1', 'Grade 2'],
-      ['Grade 2', 'Grade 3'],
-      ['Grade 3', 'Final Grade']
-    ];
-    var possible_grades = ['A', 'B', 'C', 'D', 'F', 'W'];
-    column_pairs.forEach(function(cp) {
-      possible_grades.forEach(function(g) {
-        possible_grades.forEach(function(gg) {
-          add_link(g, gg, cp[0], cp[1]);
-        })
-      })
-    });
-
-    // Add the tooltips indicating the count for each node.
-    var column_names = [
-      'Grade 1', 'Grade 2',
-      'Grade 3', 'Final Grade'
-    ];
-    column_names.forEach(function(cn) { 
-      possible_grades.forEach(function(g) {
-        if(cn != selected_column) {
-          var count = get_count([
-            {
-              'grade': g,
-              'column_name': cn
-            },
-            {
-              'grade': grade,
-              'column_name': selected_column
-            }
-          ]);
-          if(count > 0) {
-            var current_button = button_info.filter(function(bi) {
-              return ((bi.grade == g) &&
-                (bi.column_name == cn))
-            })[0];
-            d3.select(".container").append("div")
-                .attr("class", "tooltip")
-                .html(count)
-                .style('left', `${current_button.x + 0.35*button_width}px`)
-                .style('top', `${current_button.y - 0.5*button_height}px`)
-          }
+    // Set up the descriptive text with location based on just
+    // the first four terms in button_info.
+  svg
+    .style('overflow', 'visible')
+    .append('g')
+    .selectAll('text')
+    .data(button_info.slice(0,4))
+    .enter().append('text')
+    .attr('fill', 'currentColor')
+    .attr('text-anchor', 'middle')
+    .attr('x', d => d.x-10)
+    .attr('y', text_pad)
+    .attr("text-decoration","underline")
+    .text(function(o,i) {
+        if(i < 3){
+        return 'Grade ' + (i+1).toString()
         }
-      })
+        else {
+        return 'Final Grade'
+        }
     });
-    // Add the links!
-    svg
-      .selectAll('path')
-      .data(link_data)
-      .enter().append('path')
-      .attr('d', link)
-      .attr("stroke", "currentColor")
-      .attr("stroke-width", d=>d.width)
-      .attr("fill",'none')
-      .attr('class', 'link')
-      .style('opacity', 0.8);
-  })
-  // Just remove some stuff on mouseleave.
-  .on('mouseleave', function() {
-    link_data = [];
-    svg.selectAll('.link').remove();
-    d3.selectAll('div.tooltip').remove();
-  });
+
+    // Lay the buttons down within the container on
+    // top of the SVG.
+  container
+    .selectAll('button') 
+    .data(button_info)
+    .enter().append('button')
+    .attr('class', 'btn btn-secondary')
+    .style('position', 'absolute')
+    .style('top', d => `${(d.y - button_height/2)}px`)
+    .style('left', d => `${(d.x - button_width/2)}px`)
+    //   .style('padding-left', '15px')
+    //   .style('padding-right', '15px')
+    .style('height', `${button_height}px`)
+    .style('width', '44px')
+    .text(d => d.grade)
+    .attr('id', d => d.id)
+
+    // Respone to mouseenter.
+    .on('pointerenter', function(e, d) {
+        // Clean up
+        link_data = [];
+        svg.selectAll('.link').remove();
+        d3.selectAll('div.tooltip').remove();
+
+        // Figure out which button we're on, use it to define the
+        // global_filter, the total number of selected students, and
+        // the button_info itself (for placing the tooltip).
+        var grade = d.grade;
+        var selected_column = d.column_name;
+        global_filter = {'grade': grade, 'column_name': selected_column};
+        total = get_count([global_filter]);
+        var selected_button = button_info.filter(function(bi) {
+        return ((bi.grade == grade) &&
+            (bi.column_name == selected_column))
+        })[0];
+
+        // Add the tooltip for the selected button.
+        container.append("div")
+            .attr("class", "tooltip")
+            .html(total)
+            .style('left', `${selected_button.x + 0.35*button_width}px`)
+            .style('top', `${selected_button.y - 0.5*button_height}px`)
+
+        // Compute and add the links. It's small enough that we'll
+        // just iterate over all 3x6x6=108 possible links and add
+        // one only when the count is positive.
+        var column_pairs = [
+        ['Grade 1', 'Grade 2'],
+        ['Grade 2', 'Grade 3'],
+        ['Grade 3', 'Final Grade']
+        ];
+        var possible_grades = ['A', 'B', 'C', 'D', 'F', 'W'];
+        column_pairs.forEach(function(cp) {
+        possible_grades.forEach(function(g) {
+            possible_grades.forEach(function(gg) {
+            add_link(g, gg, cp[0], cp[1]);
+            })
+        })
+        });
+
+        // Add the tooltips indicating the count for each node.
+        var column_names = [
+        'Grade 1', 'Grade 2',
+        'Grade 3', 'Final Grade'
+        ];
+        column_names.forEach(function(cn) { 
+        possible_grades.forEach(function(g) {
+            if(cn != selected_column) {
+            var count = get_count([
+                {
+                'grade': g,
+                'column_name': cn
+                },
+                {
+                'grade': grade,
+                'column_name': selected_column
+                }
+            ]);
+            if(count > 0) {
+                var current_button = button_info.filter(function(bi) {
+                return ((bi.grade == g) &&
+                    (bi.column_name == cn))
+                })[0];
+                d3.select(".container").append("div")
+                    .attr("class", "tooltip")
+                    .html(count)
+                    .style('left', `${current_button.x + 0.35*button_width}px`)
+                    .style('top', `${current_button.y - 0.5*button_height}px`)
+            }
+            }
+        })
+        });
+        // Add the links!
+        svg
+        .selectAll('path')
+        .data(link_data)
+        .enter().append('path')
+        .attr('d', link)
+        .attr("stroke", "currentColor")
+        .attr("stroke-width", d=>d.width)
+        .attr("fill",'none')
+        .attr('class', 'link')
+        .style('opacity', 0.8);
+    })
+    // Just remove some stuff on mouseleave.
+    .on('mouseleave', function() {
+        link_data = [];
+        svg.selectAll('.link').remove();
+        d3.selectAll('div.tooltip').remove();
+    });
 }
 
 
